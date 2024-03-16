8564c4798c269a20ac7c070cb8efbf7bff8f5f96
Shiva Subramanyam <107316558+Shiva-ml@users.noreply.github.com>
2024-03-16 13:00:51 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,7 +87,7 @@ export default function Page() {
             </div>
           </div>
 
-          <Avatar className="size-45">
+          <Avatar className="size-42">
             <AvatarImage alt={RESUME_DATA.name} src={RESUME_DATA.avatarUrl} />
             <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
           </Avatar>
