a616412071c91deadb2bf02609ce15109fc329d1
Shiva Subramanyam <107316558+Shiva-ml@users.noreply.github.com>
2024-03-16 18:10:26 +0530

    Update avatar.tsx

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -12,7 +12,7 @@ const Avatar = React.forwardRef<
   <AvatarPrimitive.Root
     ref={ref}
     className={cn(
-      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-xl",
+      "relative flex h-50 w-40 shrink-0 overflow-hidden rounded-xl",
       className,
     )}
     {...props}
