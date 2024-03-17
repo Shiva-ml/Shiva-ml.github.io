34b2659d77dc5a7a5df255e48561a6555d8a304b
Shiva Subramanyam <107316558+Shiva-ml@users.noreply.github.com>
2024-03-17 19:34:51 +0530

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -62,15 +62,15 @@ export const RESUME_DATA = {
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
+      company: "Center for innovation and Product development",
+      link: "",
+      badges: ["Software dev for design", "Optimization"," Mathematics"],
+      title: "Consultant - Research",
       logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      start: "May 2022",
+      end: "Dec 2020",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "At CIPD, a MSME business incubator, I developed software dev tools and methods to support nature-inspired designs for hardcore product startups. I did 3 things end to end. 1)  A skeletal extraction tool and method to mimic bone's natural structure for aerospace design.  2) A GUI to extract design parameters from 2D structure images and visualize properties' responses to parameter changes. 3) Showcased "e" as a good metric to pick the best lattice structure from 1000’s for microstructure design.",
     },
     {
       company: "Clevertech",
